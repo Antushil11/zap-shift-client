9b652f3a9d45ab5d79a05b8426c87f7df75550e5
ANTU SHIL <anturoy084@gmail.com>
2025-11-19 21:23:37 +0600

    first commit

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div>
+
+
+            <Navbar></Navbar>
+
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default RootLayout;

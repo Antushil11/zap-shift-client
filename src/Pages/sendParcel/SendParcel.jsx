90b4fffd31e85e92fcde30a8f331c5c6d48f26c7
ANTU SHIL <anturoy084@gmail.com>
2025-11-27 21:03:45 +0600

    added sender parcel

--- src/Pages/sendParcel/SendParcel.jsx
+++ src/Pages/sendParcel/SendParcel.jsx
@@ -1,11 +1,176 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const SendParcel = () => {
-    return (
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+  } = useForm();
+
+  const handleSendParcel = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2 className="text-5xl font-bold">Send A Parcel</h2>
+      <form
+        onSubmit={handleSubmit(handleSendParcel)}
+        className="mt-12 p-4 text-black"
+      >
+        {/* parcel type */}
         <div>
-            
+          <label className="label mr-4">
+            <input
+              type="radio"
+              {...register("parcelType")}
+              value="document"
+              className="radio"
+              defaultChecked
+            />
+            Document
+          </label>
+
+          <label className="label">
+            <input
+              type="radio"
+              {...register("parcelType")}
+              value="non-document"
+              className="radio"
+              defaultChecked
+            />
+            Non-Document
+          </label>
         </div>
-    );
+
+        {/* parcel info wight*/}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12 my-8">
+          <fieldset className="fieldset">
+            <label className="label">Parcel name</label>
+            <input
+              type="text"
+              {...register("parcelName")}
+              className="input w-full"
+              placeholder="Parcel Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <label className="label">Parcel Weight (kg)</label>
+            <input
+              type="number"
+              {...register("parcelWeight")}
+              className="input w-full"
+              placeholder="Parcel Weight"
+            />
+          </fieldset>
+        </div>
+
+        {/* two colum */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12 my-8">
+          {/* sender info details */}
+
+          <fieldset className="fieldset">
+            <h4 className="text-2xl font-semibold">Sender Details</h4>
+            {/* sender name */}
+            <label className="label">Sender Name</label>
+            <input
+              type="text"
+              {...register("senderName")}
+              className="input w-full"
+              placeholder="Sender Name"
+            />
+
+            {/* sender Email */}
+            <label className="label">Sender Email</label>
+            <input
+              type="email"
+              {...register("senderEmail")}
+              className="input w-full"
+              placeholder="Sender Email"
+            />
+
+            {/* sender region */}
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Sender Regions</legend>
+              <select defaultValue="Pick a region" className="select">
+                <option disabled={true}>Pick a region</option>
+                <option>Chrome</option>
+                <option>FireFox</option>
+                <option>Safari</option>
+              </select>
+              <span className="label">Optional</span>
+            </fieldset>
+
+            {/* sender District*/}
+            <label className="label mt-2">Sender District</label>
+            <input
+              type="text"
+              {...register("senderDistrict")}
+              className="input w-full"
+              placeholder="Sender District"
+            />
+
+            {/* sender address */}
+            <label className="label mt-2">Sender Address</label>
+            <input
+              type="text"
+              {...register("senderAddress")}
+              className="input w-full"
+              placeholder="Sender Address"
+            />
+          </fieldset>
+
+          {/* receiver Details */}
+          <fieldset className="fieldset">
+            <h4 className="text-2xl font-semibold">Receiver Details</h4>
+            {/* receiver name */}
+            <label className="label">Receiver Name</label>
+            <input
+              type="text"
+              {...register("receiverName")}
+              className="input w-full"
+              placeholder="Receiver Name"
+            />
+            {/* receiver Email */}
+            <label className="label">Receiver Email</label>
+            <input
+              type="email"
+              {...register("receiverEmail")}
+              className="input w-full"
+              placeholder="Receiver Email"
+            />
+
+            {/* receiver address */}
+
+            <label className="label mt-2">receiver Address</label>
+            <input
+              type="text"
+              {...register("receiverAddress")}
+              className="input w-full"
+              placeholder="Receiver Address"
+            />
+
+            {/* receiver District*/}
+            <label className="label mt-2">receiver District</label>
+            <input
+              type="text"
+              {...register("receiverDistrict")}
+              className="input w-full"
+              placeholder="Receiver District"
+            />
+          </fieldset>
+        </div>
+
+        <input
+          type="submit"
+          value="Send Parcel"
+          className="btn btn-primary text-black"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SendParcel;

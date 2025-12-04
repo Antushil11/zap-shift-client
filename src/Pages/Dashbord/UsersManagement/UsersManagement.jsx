7d180a58c7627926321f4e4eadfa6a2013ef097c
ANTU SHIL <anturoy084@gmail.com>
2025-12-05 00:42:57 +0600

    added userManagment

--- src/Pages/Dashbord/UsersManagement/UsersManagement.jsx
+++ src/Pages/Dashbord/UsersManagement/UsersManagement.jsx
@@ -1,12 +1,115 @@
-import React from 'react';
+import React from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import { FaUserShield } from "react-icons/fa";
+import { FiShieldOff } from "react-icons/fi";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const UsersManagement = () => {
-    return (
-        <div>
-            <h2 className='text-4xl'>Users Management </h2>
-            
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { refetch, data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users`);
+      return res.data;
+    },
+  });
+
+  const handleMakeUser = (user) => {
+    const roleInfo = { role: "admin" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName} Marked As Admin`,
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      }
+    });
+  };
+
+  const handleRemoveAdmin = (user) => {
+    const roleInfo = { role: "user" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName} removed for admin Admin`,
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      }
+    });
+  };
+  return (
+    <div>
+      <h2 className="text-4xl">Users Management {users.length}</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Admin Action</th>
+              <th>Other Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user.photoURL}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.displayName}</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    <button onClick={() => handleRemoveAdmin(user)} className="btn bg-red-600">
+                      <FiShieldOff />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeUser(user)}
+                      className="btn bg-green-600"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </td>
+                <th>Action</th>
+              </tr>
+            ))}
+          </tbody>
+          {/* foot */}
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default UsersManagement;
